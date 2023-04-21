410aaaeb30b2e636e0a516c8a30d744c98a2fdb3
Valera Komissarenko <nuberman88@gmail.com>
2023-04-21 09:59:15 +0400

    installed gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ import styles from './gulp/compileStyles.mjs';
 import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
 import js from './gulp/compileScripts.mjs';
 import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
+import ghPages from 'gulp-gh-pages';
 
 const server = browserSync.create();
 const streamStyles = () => styles().pipe(server.stream());
@@ -42,5 +43,10 @@ const refresh = (done) => {
 const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg));
 const dev = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg), syncServer);
 const start = gulp.series(clean, copy, sprite, gulp.parallel(styles, js), syncServer);
+
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+      .pipe(ghPages());
+});
 
 export { createWebp as webp, build, start, dev};
