12a918071c985c1afe4ce3c6542665d733c1b42f
Valera Komissarenko <nuberman88@gmail.com>
2023-04-25 03:35:38 +0400

    fixed a little

--- source/js/utils/sort-advantages.js
+++ source/js/utils/sort-advantages.js
@@ -1,35 +1,38 @@
 const items = document.querySelectorAll('.advantages__item');
 const central = document.querySelector('.advantages__item--center');
 const list = document.querySelector('.advantages__list');
+const usuals = [];
 
-items.forEach((elem) => {
-  if (!elem.classList.contains('advantages__item--center')) {
-    elem.classList.add('advantages__item--usual');
-  }
-});
+if (items && central && list) {
+  items.forEach((elem) => {
+    if (!elem.classList.contains('advantages__item--center')) {
+      elem.classList.add('advantages__item--usual');
+      usuals.push(elem);
+    }
+  });
 
-const usuals = document.querySelectorAll('.advantages__item--usual');
+  const sortedElements = [];
 
-const sortedElements = [];
+  usuals.forEach((element, index) => {
+    if (index < 2) {
+      sortedElements.push(element);
+    }
+  });
 
-usuals.forEach((element, index) => {
-  if (index < 2) {
-    sortedElements.push(element);
-  }
-});
+  // центральный элемент добавляется в дерево третьим по счету. нужно для положения в сетке
+  sortedElements.push(central);
 
-sortedElements.push(central);
+  usuals.forEach((element, index) => {
+    if (index >= 2) {
+      sortedElements.push(element);
+    }
+  });
 
-usuals.forEach((element, index) => {
-  if (index >= 2) {
-    sortedElements.push(element);
-  }
-});
+  sortedElements.forEach((element) => {
+    element.replaceWith(element);
+  });
 
-sortedElements.forEach((element) => {
-  element.replaceWith(element);
-});
-
-sortedElements.forEach((element) => {
-  list.appendChild(element);
-});
+  sortedElements.forEach((element) => {
+    list.appendChild(element);
+  });
+}
